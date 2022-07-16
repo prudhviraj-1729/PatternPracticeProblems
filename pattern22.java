c34a05eb0fbb988d51902b0078ef61f17ac90f23
prudhviraj-1729 <prudhvirajchiluka1999@gmail.com>
2022-07-16 20:21:22 +0530

    Correct Count Added Pattern29 from ReadMe

---
+++ pattern22.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class pattern22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int k = i;
+            for (int j = 1; j <= i; j++) {
+                if (j == 1) {
+                    System.out.print(i);
+                }
+                else if (j % 2 == 0) {
+                    //                  k = k + n + j - 2 * (i -2 );
+                    //k = k + n + 2 - 2 * (i-2);
+                    k = k + n - 2 * (i-3);
+                    System.out.print(k);
+                }
+                else {
+                    k = k + 2 * (i-j+1);
+                    System.out.print(k);
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
